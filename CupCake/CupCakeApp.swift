df6ae2aab44c97aad520e14fce9dad12c1e9fa5d
Omidiora Emmanuel <omidioraemmanuel@gmail.com>
2026-02-19 22:50:52 +0100

    Initial Commit

---
+++ CupCake/CupCakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeApp.swift
+//  CupCake
+//
+//  Created by Omidiora Emmanuel on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
